3980bc1dc3edf57f0f062eccca0e7f00e3306a66
Timilde <Timmy-95.matildemaibaze@gmail.com>
2025-03-04 16:17:38 +0200

    primeiro commit

---
+++ app.js
@@ -0,0 +1,9 @@
+alert ('Olá bradinha');
+alert('Seja bem vindo ao meu jogo');
+let numeroSecreto =13;
+let adivinha = prompt ('Escolha um numero entre 1 e 20');
+if (numeroSecreto == adivinha) {
+    console.log('Parabéns. Descobriste o número secreto (13) ');
+} else {
+    alert ('O número é errado');
+}
