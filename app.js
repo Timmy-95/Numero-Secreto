9860692c8de0a25aa63fcdaafcc39ec2799adba3
Klayton Massango <kmassango@transcom.co.mz>
2025-03-05 14:45:40 +0200

    Alteracao do numero secreto

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 alert ('Olá bradinha');
 alert('Seja bem vindo ao meu jogo');
-let numeroSecreto =13;
+let numeroSecreto =15;
 let adivinha = prompt ('Escolha um numero entre 1 e 20');
 if (numeroSecreto == adivinha) {
     console.log('Parabéns. Descobriste o número secreto (13) ');
